262d250f9e41178858914ccef4febf72d21db895
Tim van den Bosch <mondayrunner@users.noreply.github.com>
2021-03-25 11:56:53 +0100

    Update default.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -54,6 +54,10 @@ export default {
 </script>
 
 <style>
+body {
+  background-color: lightsteelblue;
+}
+
 nav.navbar {
   box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06) !important;
 }
